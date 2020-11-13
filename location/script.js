5782924012ea8c402fd4a57389d11c52449cf3ca
wonghongkeat <hongkeat1991@gmail.com>
2020-11-13 07:27:54 +0000

    successfully added map

---
+++ location/script.js
@@ -0,0 +1,12 @@
+let singapore = [ 1.29,103.85]; // #1 Singapore latlng
+let map = L.map('map').setView(singapore, 12); // #2 Set the center point
+
+// setup the tile layers
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
+}).addTo(map);
