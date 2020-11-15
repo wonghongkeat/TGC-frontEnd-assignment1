64454168f65bbbd989a68d2127546efe401f7141
wonghongkeat <hongkeat1991@gmail.com>
2020-11-15 05:58:27 +0000

    Addtion:
    added location marker on map.
    added additional html page for bootstrap tryout.

--- Assignment1/location.js
+++ Assignment1/location.js
@@ -1,6 +1,23 @@
 let singapore = [ 1.29,103.85];
 let map = L.map('map').setView(singapore, 12); 
 
+//markers
+let location1 = L.marker([1.3470481,103.8738599]);
+location1.addTo(map);
+location1.bindPopup(`<h2>Serangoon Sunhine Park<h2>
+<p>Classes available</p>
+<ul>
+<li>Tue 1600</li>
+</ul>`);
+
+let location2 = L.marker([1.3158251,103.8561832]);
+location2.addTo(map);
+location2.bindPopup(`<h2>Groundup SG<h2>
+<p>Classes available</p>
+<ul>
+    <li>Tue 1600</li>
+</ul>`);
+
 // setup the tile layers
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
