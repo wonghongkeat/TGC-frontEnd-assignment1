e769ecba3cd7d574a909394ca3aee866fa81fbd9
wonghongkeat <hongkeat1991@gmail.com>
2020-11-25 09:54:41 +0000

    edited:
    edited the map popup and connection to the booking form

--- location.js
+++ location.js
@@ -5,27 +5,26 @@ async function locationData() {
     for (let l in loca) {
         let locationMarker = L.marker([loca[l]["lat"], loca[l]["long"]])
         locationMarker.addTo(map);
-        locationMarker.bindPopup(`<h5> ${loca[l]["location"]}</h5>
+        locationMarker.bindPopup(`<h5 class='location'> ${loca[l]["location"]}</h5>
         <p style="font-size: 15px"> Class Timing </p>
         <ul> 
         <li style="font-size: 15px">
         ${loca[l]["time"]} 
         </li>
         </ul>
-        <button><a href="#location">Book</a></button>
+        <button id="book${l}"><a href="#location">Book</a></button>
         `)
-        // let button = document.querySelector('#book');
-        // button.addEventListener("click", function () {
-        //     alert('hello')
-        // location.href = document.querySelector('#location');
-        // document.querySelector('#location').innerHTML = document.querySelector('h5').innerHTML
-        // });
+        locationMarker.on("click", function () {
+        document.querySelector(`#book${l}`).addEventListener("click", function () {
+            document.querySelector('#location').innerHTML = loca[l]["location"]
+        });
         // locationMarker.on("click", function (event) {
         //     document.querySelector("#book").addEventListener("click", function (event) {
         //         location.href = "location.html?#form";
         //         document.querySelector('#location').innerHTML = document.querySelector('h5').innerHTML
         //     });
-    };
+    });
+    }
 };
 
 locationData();
@@ -60,34 +59,6 @@ let map = L.map('map',
         trackResize: false
     }).setView(singapore, 12);
 let link = document.querySelector('#form');
-
-//markers
-// let location1 = L.marker([1.3470481, 103.8738599]);
-// location1.addTo(map);
-// location1.bindPopup(`<h2 id='location1'>Serangoon Sunshine Park<h2>
-// <p>Classes available</p>
-// <ul>
-// <li>1600</li>
-// </ul>
-// <button id='book'>Book</button>
-// `);
-
-// location1.on("click", function (event) {
-//     document.querySelector("#book").addEventListener("click", function (event) {
-//         location.href = "location.html?#form";
-//         document.querySelector('#location').innerHTML = document.querySelector('#location1').innerHTML;
-
-//     });
-// });
-
-
-// let location2 = L.marker([1.3158251, 103.8561832]);
-// location2.addTo(map);
-// location2.bindPopup(`<h2>Groundup SG<h2>
-// <p>Classes available</p>
-// <ul>
-//     <li>Tue 1600</li>
-// </ul>`);
 
 // setup the tile layers++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
