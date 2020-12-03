2677065f22df2b6a3c5657c444e457eb08943b9b
wonghongkeat <hongkeat1991@gmail.com>
2020-12-03 07:46:10 +0000

    edited:
    edited dropdown menu with click function

---
+++ script.js
@@ -0,0 +1,8 @@
+function menuButton() {
+    let menu = document.querySelector('.nav-link2');
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
